0e137c13e4682b423a4482f9a30216d53c0c5b4b
Omega Haileyesus <omegahaile@gmail.com>
2024-11-09 19:42:45 -0800

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,8 @@ public class HelloWorld
     public static void main(String []args)
     {
         System.out.println("Hello maristians. Scola Brevis");
+        for (int i = 0; i < 5; i++){
+            System.out.println("bruh");
+        }
     }
 };
