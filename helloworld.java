e9800b84ccee6a492894d9241c031d9f7d9873cd
Omega Haileyesus <omegahaileyesus@Omegas-MBP-2.attlocal.net>
2024-11-11 19:21:38 -0800

    modifed print statement to say lebron james

--- helloworld.java
+++ helloworld.java
@@ -4,7 +4,7 @@ public class HelloWorld
     {
         System.out.println("Hello maristians. Scola Brevis");
         for (int i = 0; i < 5; i++){
-            System.out.println("bruh");
+            System.out.println("lebron james");
         }
     }
 };
